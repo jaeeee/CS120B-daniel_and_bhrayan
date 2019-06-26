efd370663369a958d2a1bf79ee8d105c6272aaa6
Daniel Kwong <dkwon014@ucr.edu>
2019-06-26 13:58:10 -0700

    dafd

--- TestingProject/source/main.c
+++ TestingProject/source/main.c
@@ -1,41 +1,48 @@
 /*	Author: dkwon014
  *  Partner(s) Name: Bhrayan Escobar
  *	Lab Section:
- *	Assignment: Lab #2 Exercise #1
+ *	Assignment: Lab #2  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-
  #include <avr/io.h>
  #ifdef _SIMULATE_
  #include "simAVRHeader.h"
  #endif
+ int main(void)
+ {
+ 	DDRA = 0x00;
+  PORTA = 0xFF;
+ 	DDRB = 0xFF;
+  PORTB = 0x00;
 
+ 	unsigned char tempA0, tempA1, tempA2, tempA3 = 0x00;
+ 	unsigned char cntavail = 4;
 
- int main(void) {
-    DDRA = 0x00;
-    PORTA = 0xFF;
-    DDRB = 0xFF;
-    PORTB = 0x00;
-    unsigned char tmp1 = 0x00;
-    unsigned char tmp2 = 0x00;
-    unsigned char tmp3 = 0x00;
-    while (1)
-    {
- 	   tmp1 = PINA & 0x01;
- 	   tmp2 = PINA & 0x02;
- 	   if( tmp1 == 0x01 && tmp2 == 0x00)
- 	   {
- 		   tmp3 = (tmp3 & 0xFE) | 0x01;
- 	   }
- 	   else
- 	   {
- 		   tmp3 = (tmp3 & 0xFE) | 0x00;
- 	   }
- 	   PORTB = tmp3;
-   }
- // }
- 	return 1;
+ 	while(1) {
+ 		tempA0 = PINA & 0x01;
+ 		tempA1 = PINA & 0x02;
+ 		tempA2 = PINA & 0x04;
+ 		tempA3 = PINA & 0x08;
+ 		if(tempA0 == 0x01) {
+ 			--cntavail;
+ 		}
+ 		if(tempA1 == 0x02) {
+ 			--cntavail;
+ 		}
+ 		if(tempA2 == 0x04) {
+ 			--cntavail;
+ 		}
+ 		if(tempA3 == 0x08) {
+ 			--cntavail;
+ 		}
+    if (cntavail == 0) {
+      cntavail = 0x80;
+    }
+ 		PORTC = cntavail;
+    cntavail = 4;
+ 	}
+
  }
