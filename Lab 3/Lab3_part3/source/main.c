412846377fbd7281820468c98013c0ae18f08e0d
Daniel Kwong <dkwon014@ucr.edu>
2019-06-26 00:19:37 -0700

    ex3 description

--- Lab 3/Lab3_part3/source/main.c
+++ Lab 3/Lab3_part3/source/main.c
@@ -1,8 +1,10 @@
 /*	Author: dkwon014
  *  Partner(s) Name: Bhrayan Escobar
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Assignment: Lab #3  Exercise #3
+ *	Exercise Description: In addition to the above, PA4 is 1 if a key is in the ignition,
+ *   PA5 is one if a driver is seated, and PA6 is 1 if the driver's seatbelt is fastened.
+ *  PC7 should light a "Fasten seatbelt" icon if a key is in the ignition, the driver is seated, but the belt is not fastened.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
