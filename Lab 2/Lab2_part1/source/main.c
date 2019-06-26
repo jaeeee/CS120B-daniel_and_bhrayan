856a81abdeb05c6ebf1a484c41f99e71d46687cb
Daniel Kwong <dkwon014@ucr.edu>
2019-06-26 13:42:33 -0700

    wtf

--- Lab 2/Lab2_part1/source/main.c
+++ Lab 2/Lab2_part1/source/main.c
@@ -15,27 +15,27 @@
 
 
  int main(void) {
-    DDRA = 0x00;
-    PORTA = 0xFF;
-    DDRB = 0xFF;
-    PORTB = 0x00;
-    unsigned char tmp1 = 0x00;
-    unsigned char tmp2 = 0x00;
-    unsigned char tmp3 = 0x00;
-    while (1)
-    {
- 	   /*tmp1 = PINA & 0x01;
- 	   tmp2 = PINA & 0x02;
- 	   if( tmp1 == 0x01 && tmp2 == 0x00)
- 	   {
- 		   tmp3 = (tmp3 & 0xFE) | 0x01;
- 	   }
- 	   else
- 	   {
- 		   tmp3 = (tmp3 & 0xFE) | 0x00;
- 	   }
- 	   PORTB = tmp3;
-   }*/
- }
+ //    DDRA = 0x00;
+ //    PORTA = 0xFF;
+ //    DDRB = 0xFF;
+ //    PORTB = 0x00;
+ //    unsigned char tmp1 = 0x00;
+ //    unsigned char tmp2 = 0x00;
+ //    unsigned char tmp3 = 0x00;
+ //    while (1)
+ //    {
+ // 	   tmp1 = PINA & 0x01;
+ // 	   tmp2 = PINA & 0x02;
+ // 	   if( tmp1 == 0x01 && tmp2 == 0x00)
+ // 	   {
+ // 		   tmp3 = (tmp3 & 0xFE) | 0x01;
+ // 	   }
+ // 	   else
+ // 	   {
+ // 		   tmp3 = (tmp3 & 0xFE) | 0x00;
+ // 	   }
+ // 	   PORTB = tmp3;
+ //   }
+ // }
  	return 1;
  }
