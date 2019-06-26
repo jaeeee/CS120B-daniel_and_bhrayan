38ab34942a26286feada4546edd0fdc3f568faff
Daniel Kwong <dkwon014@ucr.edu>
2019-06-26 13:53:04 -0700

    yes

--- TestingProject/source/main.c
+++ TestingProject/source/main.c
@@ -1,23 +1,41 @@
 /*	Author: dkwon014
  *  Partner(s) Name: Bhrayan Escobar
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #2 Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+ #include <avr/io.h>
+ #ifdef _STIMLATE_
+ #include "simAVRHeader.h"
+ #endif
 
-    /* Insert your solution below */
-    while (1) {
 
-    }
-    return 1;
-}
+ int main(void) {
+    DDRA = 0x00;
+    PORTA = 0xFF;
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    unsigned char tmp1 = 0x00;
+    unsigned char tmp2 = 0x00;
+    unsigned char tmp3 = 0x00;
+    while (1)
+    {
+ 	   tmp1 = PINA & 0x01;
+ 	   tmp2 = PINA & 0x02;
+ 	   if( tmp1 == 0x01 && tmp2 == 0x00)
+ 	   {
+ 		   tmp3 = (tmp3 & 0xFE) | 0x01;
+ 	   }
+ 	   else
+ 	   {
+ 		   tmp3 = (tmp3 & 0xFE) | 0x00;
+ 	   }
+ 	   PORTB = tmp3;
+   }
+ // }
+ 	return 1;
+ }
