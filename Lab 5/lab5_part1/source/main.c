dd1910e2e1a3ba458f30be18f5f805ae5c18853a
Daniel Kwong <dkwon014@ucr.edu>
2019-07-01 12:28:40 -0700

    update main.c

--- Lab 5/lab5_part1/source/main.c
+++ Lab 5/lab5_part1/source/main.c
@@ -1,10 +1,12 @@
 /*	Author: dkwon014
  *  Partner(s) Name: Bhrayan Escobar
  *	Lab Section:
- *	Assignment: Lab #5  Exercise #1
- *	Exercise Description: In addition to the above, PA4 is 1 if a key is in the ignition,
- *   PA5 is one if a driver is seated, and PA6 is 1 if the driver's seatbelt is fastened.
- *  PC7 should light a "Fasten seatbelt" icon if a key is in the ignition, the driver is seated, but the belt is not fastened.
+ *	Assignment: Lab #3  Exercise #2
+ *	Exercise Description: A car has a fuel-level sensor that sets PA3..PA0 to a value between 0 (empty) and 15 (full).
+ *  A series of LEDs connected to PC5..PC0 should light to graphically indicate the fuel level.
+ *  If the fuel level is 1 or 2, PC5 lights. If the level is 3 or 4, PC5 and PC4 light.
+ *  Level 5-6 lights PC5..PC3. 7-9 lights PC5..PC2. 10-12 lights PC5..PC1. 13-15 lights PC5..PC0.
+ *  Also, PC6 connects to a "Low fuel" icon, which should light if the level is 4 or less.  (The example below shows the display for a fuel level of 3).
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -32,34 +34,27 @@ int main(void) {
     // PORTB = 0xFF;
     PORTC = 0x00;
 		unsigned char holder = 0x00;
-    unsigned char lower = 0x00;
-    unsigned char upper = 0x00;
     /* Insert your solution below */
     while (1) {
-      lower = !PINA & 0x0F; //lower nibble
-      upper = !PINA & 0xF0; //upper nibble
 			holder = !PINA;
-			if (((holder & 0x0F) >= 0x0D) && ((holder & 0x0F) <= 0x0F)) { //13-15
+			if ((holder >= 0x0E) && (holder <= 0x0F)) {
 				PORTC = 0x3F;
 			}
-			if (((holder & 0x0F) >= 0x0A) && ((holder & 0x0F) <= 0x0C)) { //10-12
+			if ((holder >= 0x0A) && (holder <= 0x0D)) {
 				PORTC = 0x3E;
 			}
-			if (((holder & 0x0F) >= 0x07) && ((holder & 0x0F) <= 0x09)) { //7-9
+			if ((holder >= 0x07) && (holder <= 0x09)) {
 				PORTC = 0x3C;
 			}
-			if (((holder & 0x0F) >= 0x05) && ((holder & 0x0F) <= 0x06)) { //5-6
+			if ((holder >= 0x05) && (holder <= 0x06)) {
 				PORTC = 0x38;
 			}
-			if (((holder & 0x0F) >= 0x03) && ((holder & 0x0F) <= 0x04)) { //3-4
+			if ((holder >= 0x03) && (holder <= 0x04)) {
 				PORTC = 0x70;
 			}
-			if (((holder & 0x0F) >= 0x01) && ((holder & 0x0F) <= 0x02)) { //1-2
+			if ((holder >= 0x01) && (holder <= 0x02)) {
 				PORTC = 0x60;
 			}
-      if ((GetBit(upper, 4)) && (GetBit(upper, 5)) && !(GetBit(upper, 6))) {
-        PORTC = PORTC | 0x80;
-      }
     }
     return 1;
 }
