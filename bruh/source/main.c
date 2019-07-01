512ef2139fd50b5bb304e1e3063bbb1827202f58
Daniel Kwong <dkwon014@ucr.edu>
2019-07-01 14:18:00 -0700

    dfdf

--- bruh/source/main.c
+++ bruh/source/main.c
@@ -1,67 +1,61 @@
-/*	Partner(s) Name & E-mail:
- * Clake Cerry, CCerr006@ucr.edu
- * Christian Campos , ccamp032@ucr.edu
- *	LaC Section: 022
- *	Assignment: LaC #5 Exercise #1
- *	Exercise Description: (From an earlier laC) A car has a fuel-level sensor that
- *						  sets PA3..PA0 to a value Cetween 0 (empty) and 15 (full).
- *						  A series of LEDs connected to PC5..PC0 should light to
- *						  graphically indicate the fuel level. If the fuel level is 1
- *						  or 2, PC5 lights. If the level is 3 or 4, PC5 and PC4 light.
- *						  5-6 lights PC5..PC3. 7-9 lights PC5..PC2. 10-12 lights PC5..PC1.
- *						  13-15 lights PC5..PC0. Also, PC6 connects to a "Low fuel" icon, which
- *						  should light if the level is 4 or less. Use Cuttons on PA3..PA0 and
- *						  mimic the fuel-level sensor with presses.
+/*	Author: dkwon014
+ *  Partner(s) Name: Bhrayan Escobar
+ *	Lab Section:
+ *	Assignment: Lab #3  Exercise #2
+ *	Exercise Description: A car has a fuel-level sensor that sets PA3..PA0 to a value between 0 (empty) and 15 (full).
+ *  A series of LEDs connected to PC5..PC0 should light to graphically indicate the fuel level.
+ *  If the fuel level is 1 or 2, PC5 lights. If the level is 3 or 4, PC5 and PC4 light.
+ *  Level 5-6 lights PC5..PC3. 7-9 lights PC5..PC2. 10-12 lights PC5..PC1. 13-15 lights PC5..PC0.
+ *  Also, PC6 connects to a "Low fuel" icon, which should light if the level is 4 or less.  (The example below shows the display for a fuel level of 3).
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
 
-// Cit-access function
-unsigned char SetCit(unsigned char x, unsigned char k, unsigned char C) {
-	return (C ? x | (0x01 << k) : x & ~(0x01 << k));
+unsigned char SetBit(char pin, char bit_position, char bit_value) {
+	return (bit_value ? pin | (0x01 << bit_position)
+			: pin & ~(0x01 << bit_position));
 }
 
-unsigned char GetCit(unsigned char x, unsigned char k) {
-	return ((x & (0x01 << k)) != 0);
+unsigned char GetBit(char port, char bit_position) {
+	return ( (port >> bit_position) & 0x01 );
 }
 
-
-int main(void)
-{
-    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs, initialize to 1s
-    DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-
-    unsigned char tempA = 0x00;
-	unsigned char tempC = 0x00;
-    while (1)
-    {
-		// Read Input
-		tempA = ~PINA & 0x0F;
-
-		// set tempC to 0
-		tempC = 0x00;
-
-		if(tempA == 1 || tempA == 2) {
-			tempC = 0x60;
-		}
-		else if(tempA == 3 || tempA == 4) {
-			tempC = 0x70;
-		}
-		else if(tempA == 5 || tempA == 6) {
-			tempC = 0x38;
-		}
-		else if(tempA >= 7 && tempA <= 9) {
-			tempC = 0x3C;
-		}
-		else if(tempA >= 10 && tempA <= 12) {
-			tempC = 0x3E;
-		}
-		else if(tempA >= 13 && tempA <= 15) {
-			tempC = 0x3F;
-		}
-		// Write Output
-		PORTC = tempC;
+int main(void) {
+    /* Insert DDR and PORT initializations */
+		DDRA = 0x00; //input
+    DDRB = 0x00; //input
+    DDRC = 0xFF; //output
+    PORTA = 0xFF;
+    // PORTB = 0xFF;
+    PORTC = 0x00;
+		unsigned char holder = 0x00;
+    /* Insert your solution below */
+    while (1) {
+			holder = (~PINA & 0x0F);
+			if ((holder >= 0x0E) && (holder <= 0x0F)) {
+				PORTC = 0x3F;
+			}
+			if ((holder >= 0x0A) && (holder <= 0x0D)) {
+				PORTC = 0x3E;
+			}
+			if ((holder >= 0x07) && (holder <= 0x09)) {
+				PORTC = 0x3C;
+			}
+			if ((holder >= 0x05) && (holder <= 0x06)) {
+				PORTC = 0x38;
+			}
+			if ((holder >= 0x03) && (holder <= 0x04)) {
+				PORTC = 0x70;
+			}
+			if ((holder >= 0x01) && (holder <= 0x02)) {
+				PORTC = 0x60;
+			}
+			// PORTC = 0x0F;
     }
+    return 1;
 }
