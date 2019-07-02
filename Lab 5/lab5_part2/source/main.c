05dba9e4a13c4dfda3a0e9b195da0abc1a759072
Daniel Kwong <dkwon014@ucr.edu>
2019-07-02 12:44:24 -0700

    awf

--- Lab 5/lab5_part2/source/main.c
+++ Lab 5/lab5_part2/source/main.c
@@ -21,11 +21,11 @@ case START:
 state = INIT;
 break;
 case INIT:
-if (~PINA == 0) {
+if ((~PINA && 0x00) == 0) {
 state = RESET;
-} else if (~PINA == 1) {
+} else if ((~PINA && 0x01) == 1) {
 state = INCREMENT;
-} else if (~PINA == 2) {
+} else if ((~PINA && 0x02) == 2) {
 state = DECREMENT;
 } else {
 state = INIT;
@@ -61,7 +61,7 @@ holder--;
 }
 break;
 }
-PORTC = holder;
+// PORTC = holder;
 
 
 
@@ -75,7 +75,7 @@ holder = 7;
     while (1) {
 //	holder = 7;
 	tick();
-	// PORTC = holder;
+	PORTC = holder;
     }
     return 1;
 }
