1859c3a45136b266b59a8593369b59467cced3a7
Daniel Kwong <dkwon014@ucr.edu>
2019-07-03 13:02:06 -0700

    ours

--- Lab 6/lab6_part2/source/main.c
+++ Lab 6/lab6_part2/source/main.c
@@ -1,6 +1,17 @@
+/*	Author: dkwon014
+ *  Partner(s) Name: Bhrayan Escobar
+ *	Lab Section:
+ *	Assignment: Lab #6 Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 #include <avr/io.h>
 #include <avr/interrupt.h>
-
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
 
 volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
 
@@ -8,44 +19,27 @@ volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer
 unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
 unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
 
+enum STATES { START, LIGHT_1, LIGHT_2, LIGHT_3, PAUSE } state;
+unsigned char output = 0x00;
+unsigned char FLAGERINO;
 
-enum States {start, light1, light2, light3, wait1, wait2, wait3} state;
-unsigned char tmpB = 0x00;
-unsigned char button = 0x00;
-
+#define button (~PINA & 0x01)
 
 void TimerOn() {
-	// AVR timer/counter controller register TCCR1
-	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
-	// bit2bit1bit0=011: pre-scaler /64
-	// 00001011: 0x0B
-	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
-	// Thus, TCNT1 register will count at 125,000 ticks/s
-
-	// AVR output compare register OCR1A.
-	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
-	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
-	// So when TCNT1 register equals 125,
-	// 1 ms has passed. Thus, we compare to 125.
-	// AVR timer interrupt mask register
-	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
-
-	//Initialize avr counter
-	TCNT1=0;
-
-	_avr_timer_cntcurr = _avr_timer_M;
-	// TimerISR will be called every _avr_timer_cntcurr milliseconds
-
-	//Enable global interrupts
-	SREG |= 0x80; // 0x80: 1000000
+  TCCR1B = 0x0B;
+  OCR1A = 125;
+  TIMSK1 = 0x02;
+  TCNT1 = 0;
+  _avr_timer_cntcurr = _avr_timer_M;
+  SREG |= 0x80;
 }
 
 void TimerOff() {
-	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+  TCCR1B = 0x00;
 }
 
 void TimerISR() {
-	TimerFlag = 1;
+  TimerFlag = 1;
 }
 
 // In our approach, the C programmer does not touch this ISR, but rather TimerISR()
@@ -63,104 +57,97 @@ void TimerSet(unsigned long M) {
 	_avr_timer_M = M;
 	_avr_timer_cntcurr = _avr_timer_M;
 }
-void Tick(){
-	button = ~PINA & 0x01;
-	switch (state)
-	{
-		case start:
-			state = light1;
-			break;
-		case light1:
-			if(button) {
-				state = wait1;
-			}
-			else {
-			state = light2;
-			}
-			break;
-		case light2:
-			if(button) {
-				state = wait2;
-				}
-			else {
-				state = light3;
-			}
-			break;
-		case light3:
-			if(button) {
-				state = wait3;
-			}
-			else {
-				state = light1;
-			}
-			break;
-		case wait1:
-			if(button)
-				state = light1;
-			else
-				state = wait1;
-			break;
-		case wait2:
-			if(button)
-				state = light1;
-			else
-				state = wait2;
-			break;
-		case wait3:
-			if(button)
-			state = light1;
-			else
-				state = wait3;
-			break;
-		default:
-			state = start;
-			break;
-	}
-	switch (state){
-		case start:
-			break;
-		case light1:
-			tmpB = 0x01;
-			break;
-		case light2:
-			tmpB = 0x02;
-			break;
-		case light3:
-			tmpB = 0x04;
-			break;
-		case wait1:
-			tmpB = 0x01;
-			break;
-		case wait2:
-			tmpB = 0x02;
-			break;
-		case wait3:
-			tmpB = 0x04;
-			break;
-		default:
-			state = start;
-			break;
 
-	}
+void tick() {
+  switch(state) {
+    case START:
+    state = LIGHT_1;
+    break;
+    case LIGHT_1:
+    if (button) {
+      state = PAUSE;
+    } else {
+    state = LIGHT_2;
+  }
+    break;
+    case LIGHT_2:
+    if (button) {
+      state = PAUSE;
+    } else {
+    state = LIGHT_3;
+  }
+    break;
+    case LIGHT_3:
+    if (button) {
+      state = PAUSE;
+    } else {
+    state = LIGHT_1;
+  }
+    break;
+    case PAUSE:
+    if (!button) {
+    switch(FLAGERINO) {
+      case 1:
+      state = LIGHT_1;
+      break;
+      case 2:
+      state = LIGHT_2;
+      break;
+      case 3:
+      state = LIGHT_3;
+      break;
+    }
+  } else {
+    state = PAUSE;
+  }
+    break;
+    default:
+    state = START;
+    break;
+  }
+  switch(state) {
+    case START:
+    break;
+    case LIGHT_1:
+    output = 0x01;
+    FLAGERINO = 1;
+    break;
+    case LIGHT_2:
+    output = 0x02;
+      FLAGERINO = 2;
+    break;
+    case LIGHT_3:
+    output = 0x04;
+      FLAGERINO = 3;
+    break;
+    case PAUSE:
+    output = output;
+    break;
+    default:
+    state = START;
+    break;
+  }
 }
 
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00;
+    PORTA = 0xFF;
+    DDRC = 0xFF;
+    PORTC = 0x00;
+    TimerSet(300); //set timer here
+    TimerOn(); //turn on timer
+    state = START; //change to START state
+    // tick();
+    // whil
+    /* Insert your solution below */
+    while (1) {
+      tick();
+      while (!TimerFlag) {
 
-int main()
-{
-	DDRC = 0xFF; PORTC = 0x00; // Init port B to 0s
-	DDRA = 0x00; PORTA = 0xFF;
-	TimerSet(300);
-	TimerOn();
-	state = start;
-	while(1) {
-
-		Tick();
-		while (!TimerFlag);	// Wait 1 sec
-		TimerFlag = 0;
-		PORTC = tmpB;
-
-
-
-	}
-	return 0;
+      }
+      TimerFlag = 0;
+            PORTC = output;
+    }
+    return 1;
 }
