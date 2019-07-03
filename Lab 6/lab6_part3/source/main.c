3088d240892c7772ffacba69bc43335a08d042a8
Daniel Kwong <dkwon014@ucr.edu>
2019-07-03 13:15:14 -0700

    wt

--- Lab 6/lab6_part3/source/main.c
+++ Lab 6/lab6_part3/source/main.c
@@ -1,17 +1,57 @@
 /*	Author: dkwon014
  *  Partner(s) Name: Bhrayan Escobar
  *	Lab Section:
- *	Assignment: Lab #5 Exercise #2
+ *	Assignment: Lab #6 Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+ #include <avr/io.h>
+ #include <avr/interrupt.h>
+ #ifdef _SIMULATE_
+ #include "simAVRHeader.h"
+ #endif
 
+ volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+ // Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+ unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+ unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+ void TimerOn() {
+   TCCR1B = 0x0B;
+   OCR1A = 125;
+   TIMSK1 = 0x02;
+   TCNT1 = 0;
+   _avr_timer_cntcurr = _avr_timer_M;
+   SREG |= 0x80;
+ }
+
+ void TimerOff() {
+   TCCR1B = 0x00;
+ }
+
+ void TimerISR() {
+   TimerFlag = 1;
+ }
+
+ // In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ ISR(TIMER1_COMPA_vect) {
+ 	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+ 	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+ 	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+ 		TimerISR(); // Call the ISR that the user uses
+ 		_avr_timer_cntcurr = _avr_timer_M;
+ 	}
+ }
+
+ // Set TimerISR() to tick every M ms
+ void TimerSet(unsigned long M) {
+ 	_avr_timer_M = M;
+ 	_avr_timer_cntcurr = _avr_timer_M;
+ }
+ 
 #define A0 (~PINA & 0x01)
 #define A1 (~PINA & 0x02)
 
@@ -102,7 +142,11 @@ holder = 7;
     while (1) {
 //	holder = 7;
 	tick();
-	PORTC = holder;
+  while (!TimerFlag) {
+
+  }
+  TimerFlag = 0;
+        PORTC = holder;
     }
     return 1;
 }
