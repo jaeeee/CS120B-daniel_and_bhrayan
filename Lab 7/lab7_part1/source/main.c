5654af7ac005da02e7c25cebd75436b8ad214530
Daniel Kwong <dkwon014@ucr.edu>
2019-07-08 13:30:10 -0700

    zxczxczs

--- Lab 7/lab7_part1/source/main.c
+++ Lab 7/lab7_part1/source/main.c
@@ -20,9 +20,12 @@ volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer
 unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
 unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
 
-enum STATES { START, LIGHT_1, LIGHT_2, LIGHT_3, PAUSE } state;
-unsigned char output = 0x00;
-unsigned char FLAGERINO;
+
+#define A0 (~PINA & 0x01)
+#define A1 (~PINA & 0x02)
+
+enum STATES { START, INIT, INCREMENT, RESET, DECREMENT, WAIT1, WAIT2, WAIT3 } state;
+unsigned char holder = 0x00;
 
 #define button (~PINA & 0x01)
 
@@ -59,78 +62,102 @@ void TimerSet(unsigned long M) {
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
+
 void tick() {
-  switch(state) {
-    case START:
-    state = LIGHT_1;
-    break;
-    case LIGHT_1:
-    if (button) {
-      state = PAUSE;
-    } else {
-    state = LIGHT_2;
-  }
-    break;
-    case LIGHT_2:
-    if (button) {
-      state = PAUSE;
-    } else {
-    state = LIGHT_3;
-  }
-    break;
-    case LIGHT_3:
-    if (button) {
-      state = PAUSE;
-    } else {
-    state = LIGHT_1;
-  }
-    break;
-    case PAUSE:
-    if (!button) {
-    switch(FLAGERINO) {
-      case 1:
-      state = LIGHT_1;
-      break;
-      case 2:
-      state = LIGHT_2;
-      break;
-      case 3:
-      state = LIGHT_3;
-      break;
-    }
-    FLAGERINO = 0;
-  } else {
-    state = PAUSE;
-  }
-    break;
-    default:
-    state = START;
-    break;
-  }
-  switch(state) {
-    case START:
-    break;
-    case LIGHT_1:
-    output = 0x01;
-    FLAGERINO = 1;
-    break;
-    case LIGHT_2:
-    output = 0x02;
-      FLAGERINO = 2;
-    break;
-    case LIGHT_3:
-    output = 0x04;
-      FLAGERINO = 3;
-    break;
-    case PAUSE:
-    output = output;
-    break;
-    default:
-    state = START;
-    break;
-  }
+switch(state) {
+case START:
+state = INIT;
+break;
+case INIT:
+if (A1 && A0) {
+  state = WAIT3;
+}
+else if (A0) {
+  state = WAIT1;
+} else if (A1) {
+  state = WAIT2;
+// } else if (A1 && A0) {
+//   state = WAIT3;
+} else {
+  state = INIT;
+}
+// if ((~PINA & 0x00) == 0) {
+// state = RESET;
+// } else if ((~PINA & 0x01) == 1) {
+// state = INCREMENT;
+// } else if ((~PINA && 0x02) == 2) {
+// state = DECREMENT;
+// } else {
+// state = INIT;
+// }
+break;
+case INCREMENT:
+state = INIT;
+break;
+case RESET:
+state = INIT;
+break;
+case DECREMENT:
+state = INIT;
+break;
+case WAIT1:
+if (!A0) {
+  state = INCREMENT;
+} else {
+  state = WAIT1;
+}
+break;
+case WAIT2:
+if (!A1) {
+  state = DECREMENT;
+} else {
+  state = WAIT2;
+}
+break;
+case WAIT3:
+if ((A1) || (!A0)) {
+  state = RESET;
+} else {
+  state = WAIT3;
+// state = RESET;
+}
+break;
+// case WAIT4:
+// break;
 }
 
+switch(state) {
+case START:
+break;
+case INIT:
+break;
+case INCREMENT:
+if (holder < 9) {
+holder++;
+}
+break;
+case RESET:
+holder = 0;
+break;
+case DECREMENT:
+if (holder > 0) {
+holder--;
+}
+break;
+case WAIT1:
+break;
+case WAIT2:
+break;
+case WAIT3:
+break;
+// case WAIT4:
+// break;
+}
+// PORTC = holder;
+
+
+
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00;
@@ -148,8 +175,9 @@ int main(void) {
     /* Insert your solution below */
     while (1) {
       tick();
-            PORTC = output;
-            LCD_WriteData(9 + '0');
+            PORTC = holder;
+
+            LCD_WriteData(holder + '0');
             // LCD_DisplayString(1, "hi");
             while (!TimerFlag) {
 
