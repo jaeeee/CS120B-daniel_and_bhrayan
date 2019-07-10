39679be26387b8d5938f82b512db292997e563c5
Daniel Kwong <dkwon014@ucr.edu>
2019-07-10 12:45:48 -0700

    zxczxcz

--- Lab 9/lab9_part3/source/main.c
+++ Lab 9/lab9_part3/source/main.c
@@ -16,6 +16,44 @@
 // #ifdef _SIMULATE_
 // #include "simAVRHeader.h"
 // #endif
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+void TimerOn() {
+  TCCR1B = 0x0B;
+  OCR1A = 125;
+  TIMSK1 = 0x02;
+  TCNT1 = 0;
+  _avr_timer_cntcurr = _avr_timer_M;
+  SREG |= 0x80;
+}
+
+void TimerOff() {
+  TCCR1B = 0x00;
+}
+
+void TimerISR() {
+  TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
 
 void set_PWM(double frequency) {
   static double current_frequency;
@@ -88,7 +126,6 @@ void tick() {
 		// 	break;
 		// }
     	set_PWM(NOTES[1]);
-          asm("nop");
       	set_PWM(NOTES[3]);
             asm("nop");
         	set_PWM(NOTES[5]);
@@ -158,20 +195,14 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	PWM_on();
+  TimerSet(100);
+  TimerOff();
 	state = OFF;
 	while(1) {
-		// tick();
-    set_PWM(NOTES[1]);
-        asm("nop");
-      set_PWM(NOTES[3]);
-          asm("nop");
-        set_PWM(NOTES[5]);
-            asm("nop");
-          set_PWM(NOTES[3]);
-              asm("nop");
-            set_PWM(NOTES[1]);
-                asm("nop");
-              set_PWM(NOTES[7]);
-  asm("nop");
+		tick();
+    while (!TimerFlag) {
+
+    }
+    TimerFlag = 0;
 	}
 }
