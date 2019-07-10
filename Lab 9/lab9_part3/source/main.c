907137ad62fbecac980291c3b2cee99148002602
Daniel Kwong <dkwon014@ucr.edu>
2019-07-10 13:13:50 -0700

    dfafafa

--- Lab 9/lab9_part3/source/main.c
+++ Lab 9/lab9_part3/source/main.c
@@ -1,26 +1,55 @@
 /*	Author: dkwon014
  *  Partner(s) Name: Bhrayan Escobar
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #6 Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-//
-// #include <avr/io.h>
-// #ifdef _SIMULATE_
-// #include "simAVRHeader.h"
-// #endif
+
 volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
 
 // Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
 unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
 unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+void set_PWM(double frequency) {
+  static double current_frequency;
+  if (frequency != current_frequency) {
+    if (!frequency) { TCCR3B &= 0x08; }
+    else { TCCR3B |= 0x03; }
+    if (frequency < 0.954) { OCR3A = 0xFFFF; }
+    else if (frequency > 31250) { OCR3A = 0x0000; }
+    else { OCR3A = (short) (8000000 / (128 * frequency)) - 1; }
+    TCNT3 = 0;
+    current_frequency = frequency;
+  }
+}
+
+void PWM_on() {
+  TCCR3A = (1 << COM3A0);
+  TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+  set_PWM(0);
+}
+
+void PWM_off() {
+  TCCR3A = 0x00;
+  TCCR3B = 0x00;
+}
+#define A0 (~PINA & 0x01)
+#define A1 (~PINA & 0x02)
+
+enum STATES { START, INIT, INCREMENT, RESET, DECREMENT, WAIT1, WAIT2, WAIT3 } state;
+unsigned char holder = 0x00;
+
+#define button (~PINA & 0x01)
 
 void TimerOn() {
   TCCR1B = 0x0B;
@@ -55,92 +84,126 @@ void TimerSet(unsigned long M) {
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-void set_PWM(double frequency) {
-  static double current_frequency;
-  if (frequency != current_frequency) {
-    if (!frequency) { TCCR3B &= 0x08; }
-    else { TCCR3B |= 0x03; }
-    if (frequency < 0.954) { OCR3A = 0xFFFF; }
-    else if (frequency > 31250) { OCR3A = 0x0000; }
-    else { OCR3A = (short) (8000000 / (1280 * frequency)) - 1; }
-    TCNT3 = 0;
-    current_frequency = frequency;
-  }
-}
-
-void PWM_on() {
-  TCCR3A = (1 << COM3A0);
-  TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
-  set_PWM(0);
-}
-
-void PWM_off() {
-  TCCR3A = 0x00;
-  TCCR3B = 0x00;
-}
-
-#define button (~PINA & 0x07)
 double NOTES[8] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
-// enum STATES { OFF, ON, HOLDON, HOLDOFF, PLAY, UP, WAITUP, DOWN, WAITDOWN } state;
-enum STATES { FIRST, SECOND, THIRD, FOURTH, FIFTH, OFF } state;
-unsigned char i;
 
 void tick() {
-	switch(state) {
-    case FIRST:
-    state = SECOND;
-    break;
-    case SECOND:
-    state = THIRD;
-    break;
-    case THIRD:
-      state = FOURTH;
-    break;
-    case FOURTH:
-      state = FIFTH;
-    break;
-    case FIFTH:
-state = OFF;
-    break;
-    case OFF:
-    PWM_off();
-    break;
-  }
-  switch(state) {
-    case FIRST:
-        set_PWM(NOTES[0]);
-        break;
-    case SECOND:
-        set_PWM(NOTES[1]);
-        break;
-    case THIRD:
-          set_PWM(NOTES[2]);
-          break;
-    case FOURTH:
-          set_PWM(NOTES[3]);
-          break;
-    case FIFTH:
-        set_PWM(NOTES[4]);
-        break;
-    case OFF:
-    break;
-  }
+switch(state) {
+case START:
+state = INIT;
+break;
+case INIT:
+if (A1 && A0) {
+  state = WAIT3;
+}
+else if (A0) {
+  state = INCREMENT;
+} else if (A1) {
+  state = DECREMENT;
+// } else if (A1 && A0) {
+//   state = WAIT3;
+} else {
+  state = INIT;
+}
+break;
+case INCREMENT:
+state = INIT;
+break;
+case RESET:
+state = INIT;
+break;
+case DECREMENT:
+state = INIT;
+break;
+case WAIT1:
+if (!A0) {
+  state = INCREMENT;
+} else {
+  state = WAIT1;
+}
+break;
+case WAIT2:
+if (!A1) {
+  state = DECREMENT;
+} else {
+  state = WAIT2;
+}
+break;
+case WAIT3:
+if ((A1) || (!A0)) {
+  state = RESET;
+} else {
+  state = WAIT3;
+// state = RESET;
+}
+break;
+// case WAIT4:
+// break;
 }
 
+switch(state) {
+case START:
+break;
+case INIT:
+break;
+case INCREMENT:
+if (holder < 9) {
+holder++;
+}
+break;
+case RESET:
+holder = 0;
+break;
+case DECREMENT:
+if (holder > 0) {
+holder--;
+}
+break;
+case WAIT1:
+break;
+case WAIT2:
+break;
+case WAIT3:
+break;
+// case WAIT4:
+// break;
+}
+// PORTC = holder;
+
+
+
+}
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-
-  TimerSet(100);
-  TimerOn();
-
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00;
+    PORTA = 0xFF;
+    DDRC = 0xFF;
+    PORTC = 0x00;
+    DDRD = 0xFF;
+    PORTD = 0x00;
     PWM_on();
-  // i = 0;
+    TimerSet(100); //set timer here
+    TimerOn(); //turn on timer
+    state = START; //change to START state
+    // tick();
+    // whil
+    LCD_init();
+    /* Insert your solution below */
+    LCD_Cursor(1);
+    while (1) {
+                  // LCD_Cursor(1);
+                  LCD_ClearScreen();
+      tick();
+            // PORTC = holder;
+            // // holder++;
+            set_PWM(NOTES[holder]);
+            // LCD_WriteData(holder + '0');
 
-  	state = FIRST;
-	while(1) {
-		tick();
-    while (!TimerFlag) {}
-    TimerFlag = 0;
-	}
+            // LCD_DisplayString(1, "hi");
+            // while (!TimerFlag) {
+            // }
+            while (!TimerFlag) {}
+            TimerFlag = 0;
+            // TimerFlag = 0;
+    }
+    // return 1;
 }
