20e6f9fe9a5e99acece308672b2247fedb85fd64
Daniel Kwong <dkwon014@ucr.edu>
2019-07-15 13:57:29 -0700

    dfagda

--- Lab 11/lab11_part2/source/main.c
+++ Lab 11/lab11_part2/source/main.c
@@ -1,88 +1,80 @@
+/*	Author: dkwon014
+ *  Partner(s) Name: Bhrayan Escobar
+ *	Lab Section:
+ *	Assignment: Lab #11  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#ifdef _SIMULATE_
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "timer.h"
 #include "io.c"
 #include "keypad.h"
 #include "scheduler.h"
+// #include "./.."
+// #include "io.h"
+// #include "simAVRHeader.h"
+// #include "timer.h"
+// #include "scheduler.h"
+// #include "keypad.h"
+#endif
 
-enum SM1_States{SM1_output};
-unsigned char counter = 1;
-unsigned char tmpB = 0x00;
-const unsigned char phrase[67]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','C','S','1','2','0','B',' ','i','s',' ','L','e','g','e','n','d','.','.','.','w','a','i','t',' ','f','o','r',' ','i','t',' ','D','A','R','Y','!',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+enum STATES { STATE_OUT };
 
+unsigned char holderB = 0x00;
 unsigned char i;
-int SMTick1(int state){
-	switch(state){
-		case SM1_output:
-			for(i = 1; i <= 16; i++){
-				LCD_Cursor(i);
-				LCD_WriteData(phrase[counter+i-2]);
-				if(counter+i+1 == 68){
-					counter = 1;
-				}
-			}
-			counter++;
-			break;
-		default:
-			break;
-	}
-	return state;
+const unsigned char bruh[10] = {'i', 'l', 'o', 'v', 'e', ',', ',', ',' ,'C', 'S'};
+
+int tick(int state) {
+  // unsigned char keypad_input;
+  // keypad_input = GetKeypadKey();
+  switch(state) {
+    case STATE_OUT:
+    for (i = 1; i < 10; i++) {
+      LCD_Cursor(i);
+      LCD_WriteData("HELLO WORLD!!!");
+    }
+    break;
+    }
+  return state;
 }
 
-int main()
-{
-	// Buttons PORTA[0-7], set AVR PORTA to pull down logic
-	DDRA = 0xFF; PORTA = 0x00;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
-	DDRD = 0xFF; PORTD = 0x00;
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0xFF; PORTA = 0x00;
+    DDRB = 0xFF; PORTB= 0x00;
+    DDRC = 0xF0; PORTC = 0x0F;
+    DDRD = 0xFF; PORTD = 0x00;
 
-	// Period for the tasks
-	unsigned long int SMTick1_calc = 200;
+    // unsigned long int
+    static task task1;
+    task *tasks[] = { &task1};
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
-	//Calculating GCD
-	unsigned long int tmpGCD = 1;
+	   // Task 1
+	   task1.state = 0;//Task initial state.
+	   task1.period = 200;//Task Period.
+	   task1.elapsedTime = 200;//Task current elapsed time.
+     task1.TickFct = &tick;//Function pointer for the tick.
 
-	//Greatest common divisor for all tasks or smallest time unit for tasks.
-	unsigned long int GCD = tmpGCD;
+	   TimerSet(1);
+	   TimerOn();
+     LCD_init();
 
-	//Recalculate GCD periods for scheduler
-	unsigned long int SMTick1_period = SMTick1_calc;
-
-	//Declare an array of tasks
-	static task task1;
-	task *tasks[] = { &task1};
-	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
-
-	// Task 1
-	task1.state = 0;//Task initial state.
-	task1.period = SMTick1_period;//Task Period.
-	task1.elapsedTime = SMTick1_period;//Task current elapsed time.
-	task1.TickFct = &SMTick1;//Function pointer for the tick.
-
-
-	// Set the timer and turn it on
-	TimerSet(GCD);
-	TimerOn();
-	LCD_init();
-
-	unsigned short i; // Scheduler for-loop iterator
-	while(1) {
-		// Scheduler code
-		for ( i = 0; i < numTasks; i++ ) {
-			// Task is ready to tick
-			if ( tasks[i]->elapsedTime >= tasks[i]->period ) {
-				// Setting next state for task
-				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
-				// Reset the elapsed time for next tick.
-				tasks[i]->elapsedTime = 0;
-			}
-			tasks[i]->elapsedTime += 1;
-		}
-		while(!TimerFlag);
-		TimerFlag = 0;
-	}
-
-	// Error: Program should not exit!
-	return 0;
+     unsigned short i; // Scheduler for-loop iterator
+   	while(1) {
+   		for ( i = 0; i < numTasks; i++ ) {
+   			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
+   				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+   				tasks[i]->elapsedTime = 0;
+   			}
+   			tasks[i]->elapsedTime += 1;
+   		}
+   		while(!TimerFlag);
+   		TimerFlag = 0;
+   	}
+    return 0;
 }
