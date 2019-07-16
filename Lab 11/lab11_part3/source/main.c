46b45c2f6cba286bd076f269381dd4eb3077b971
Daniel Kwong <dkwon014@ucr.edu>
2019-07-16 13:09:20 -0700

    dgaad

--- Lab 11/lab11_part3/source/main.c
+++ Lab 11/lab11_part3/source/main.c
@@ -25,8 +25,10 @@ int tick(int state) {
   keypad_input = GetKeypadKey();
   switch(state) {
     case STATE_OUT:
-    LCD_Cursor(1);
-    LCD_DisplayString(1,"NOTHING");
+    if (keypad_input == NULL) {
+      LCD_Cursor(1);
+      LCD_DisplayString(1,"NOTHING");
+    }
     switch(keypad_input) {
       case '\0':
       			holderB = 0x1F;
