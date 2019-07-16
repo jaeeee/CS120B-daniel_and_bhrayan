99e4dd6bbc162a2e19db2c7115ef2106c0417f5d
Daniel Kwong <dkwon014@ucr.edu>
2019-07-16 12:34:29 -0700

    gdadgagga

--- Lab 11/lab11_part2/source/main.c
+++ Lab 11/lab11_part2/source/main.c
@@ -1,126 +1,87 @@
-/*	Author: dkwon014
- *  Partner(s) Name: Bhrayan Escobar
- *	Lab Section:
- *	Assignment: Lab #11  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
 #include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
+#include <avr/interrupt.h>
 #include "timer.h"
-#include "scheduler.h"
+#include "io.c"
 #include "keypad.h"
-#include "io.h"
-#endif
+#include "scheduler.h"
 
-enum STATES { STATE_OUT };
-
-unsigned char holderB = 0x00;
-
-int tick(int state) {
-  unsigned char keypad_input;
-  keypad_input = GetKeypadKey();
-  switch(state) {
-    case STATE_OUT:
-    switch(keypad_input) {
-      case '\0':
-      			holderB = 0x1F;
-      			break;
-      			case '1':
-      			holderB = 0x01;
-      			break;
-      			case '2':
-      			holderB = 0x02;
-      			break;
-      			case '3':
-      			holderB = 0x03;
-      			break;
-      			case '4':
-      			holderB = 0x04;
-      			break;
-      			case '5':
-      			holderB = 0x05;
-      			break;
-      			case '6':
-      			holderB = 0x06;
-      			break;
-      			case '7':
-      			holderB = 0x07;
-      			break;
-      			case '8':
-      			holderB = 0x08;
-      			break;
-      			case '9':
-      			holderB = 0x09;
-      			break;
-      			case 'A':
-      			holderB = 0x0A;
-      			break;
-      			case 'B':
-      			holderB = 0x0B;
-      			break;
-      			case 'C':
-      			holderB = 0x0C;
-      			break;
-      			case 'D':
-      			holderB = 0x0D;
-      			break;
-      			case '*':
-      			holderB = 0x0E;
-      			break;
-      			case '0':
-      			holderB = 0x00;
-      			break;
-      			case '#':
-      			holderB = 0x0F;
-      			break;
-      			default:
-      			holderB = 0x1B;
-      			break;
-    }
-    state = STATE_OUT;
-    PORTB = holderB;
-    break;
-  }
-  return state;
+enum SM1_States{SM1_output};
+unsigned char counter = 1;
+unsigned char tmpB = 0x00;
+const unsigned char phrase[67]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','C','S','1','2','0','B',' ','i','s',' ','L','e','g','e','n','d','.','.','.','w','a','i','t',' ','f','o','r',' ','i','t',' ','D','A','R','Y','!',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+unsigned char i;
+int SMTick1(int state){
+	switch(state){
+		case SM1_output:
+			for(i = 1; i <= 16; i++){
+				LCD_Cursor(i);
+				LCD_WriteData(phrase[counter+i-2]);
+				if(counter+i+1 == 68){
+					counter = 1;
+				}
+			}
+			counter++;
+			break;
+		default:
+			break;
+	}
+	return state;
 }
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    // DDRA = 0xFF; PORTA = 0x00;
-    //NEW DDRA
-    DDRA = 0xF0; PORTA = 0x0F;
-    DDRB = 0xFF; PORTB= 0x00;
-    // DDRC = 0xF0; PORTC = 0x0F;
-    DDRD = 0xFF; PORTD = 0x00;
 
-    // unsigned long int
-    static task task1;
-    task *tasks[] = { &task1};
-    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+int main()
+{
+	// Buttons PORTA[0-7], set AVR PORTA to pull down logic
+	DDRA = 0xFF; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+	DDRD = 0xFF; PORTD = 0x00;
 
-	   // Task 1
-	   task1.state = 0;//Task initial state.
-	   task1.period = 2;//Task Period.
-	   task1.elapsedTime = 2;//Task current elapsed time.
-     task1.TickFct = &tick;//Function pointer for the tick.
+	// Period for the tasks
+	unsigned long int SMTick1_calc = 200;
 
-	   TimerSet(10);
-	   TimerOn();
+	//Calculating GCD
+	unsigned long int tmpGCD = 1;
 
-     unsigned short i; // Scheduler for-loop iterator
-   	while(1) {
-   		for ( i = 0; i < numTasks; i++ ) {
-   			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
-   				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
-   				tasks[i]->elapsedTime = 0;
-   			}
-   			tasks[i]->elapsedTime += 1;
-   		}
-   		while(!TimerFlag);
-   		TimerFlag = 0;
-   	}
-    return 0;
+	//Greatest common divisor for all tasks or smallest time unit for tasks.
+	unsigned long int GCD = tmpGCD;
+
+	//Recalculate GCD periods for scheduler
+	unsigned long int SMTick1_period = SMTick1_calc;
+
+	//Declare an array of tasks
+	static task task1;
+	task *tasks[] = { &task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	// Task 1
+	task1.state = 0;//Task initial state.
+	task1.period = SMTick1_period;//Task Period.
+	task1.elapsedTime = SMTick1_period;//Task current elapsed time.
+	task1.TickFct = &SMTick1;//Function pointer for the tick.
+
+
+	// Set the timer and turn it on
+	TimerSet(GCD);
+	TimerOn();
+	LCD_init();
+
+	unsigned short i; // Scheduler for-loop iterator
+	while(1) {
+		// Scheduler code
+		for ( i = 0; i < numTasks; i++ ) {
+			// Task is ready to tick
+			if ( tasks[i]->elapsedTime >= tasks[i]->period ) {
+				// Setting next state for task
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				// Reset the elapsed time for next tick.
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;
+	}
+
+	// Error: Program should not exit!
+	return 0;
 }
